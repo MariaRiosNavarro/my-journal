89cd7b2f1979bcdd9e02679be9a12beba18ecc5f
Maria Rios Navarro <propscode254@gmail.com>
2023-05-20 17:26:57 +0200

    Complete EntryForm.css

--- src/components/EntryForm/EntryForm.js
+++ src/components/EntryForm/EntryForm.js
@@ -2,13 +2,29 @@ import "./EntryForm.css";
 
 export default function EntryForm() {
   return (
-    <form>
-        <label></label>
-        <input></input>
-        <label></label>
-        <textarea></textarea>
-        <button></button>
-
+    <form className="entry-form">
+      <h2 className="entry-form__heading">NEW ENTRY</h2>
+      <label htmlFor="motto">Motto</label>
+      <input
+        className="entry-form__input"
+        rows={2}
+        type="text"
+        name="motto"
+        id="motto"
+        aria-describedby="place to write the motto"
+      ></input>
+      <label htmlFor="notes">Notes</label>
+      <textarea
+        className="entry-form__textarea"
+        rows={5}
+        type="text"
+        name="notes"
+        id="notes"
+        aria-describedby="place to write the journal notes"
+      ></textarea>
+      <button type="submit" className="entry-form__button">
+        Create
+      </button>
     </form>
   );
 }
