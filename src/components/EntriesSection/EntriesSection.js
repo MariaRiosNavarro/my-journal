6573f5d56804730e649e77af6c25dbfb0ae042d0
Maria Rios Navarro <propscode254@gmail.com>
2023-05-20 23:24:15 +0200

    Add Entries Styles

--- src/components/EntriesSection/EntriesSection.js
+++ src/components/EntriesSection/EntriesSection.js
@@ -11,20 +11,20 @@ export default function EntriesSection(tabname, tabnumber) {
       <TabBar />
       <EntryList>
         <Entry
-          title="Thats life in the city"
           date="27.02.2028"
+          title="Thats life in the city"
           text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum"
           icon={starfilled}
         />
         <Entry
-          title="Thats life in the city"
           date="26.02.2028"
+          title="Thats life in the city"
           text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum"
           icon={star}
         />
         <Entry
-          title="Thats life in the city"
           date="25.02.2028"
+          title="Thats life in the city"
           text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum"
           icon={star}
         />
--- src/components/Entry/Entry.js
+++ src/components/Entry/Entry.js
@@ -2,15 +2,18 @@ import "./Entry.css";
 
 export default function Entry({ title, date, text, icon }) {
   return (
-    <li className="entries-section__list-item">
-      <img
-        className="entries-section__list-item__journal-icon"
-        src={icon}
-        alt="Icon"
-      />
-      <h3 className="entries-section__list-item__journal-title">{title}</h3>
-      <p className="entries-section__list-item__journal-date">{date}</p>
-      <p className="entries-section__list-item__journal-text">{text}</p>
-    </li>
+    <>
+      <li className="entries-section__list-item">
+        <h3 className="entries-section__list-item__journal-date">{date}</h3>
+        <img
+          className="entries-section__list-item__journal-icon"
+          src={icon}
+          alt="Icon"
+        />
+        <h4 className="entries-section__list-item__journal-title">{title}</h4>
+        <p className="entries-section__list-item__journal-text">{text}</p>
+      </li>
+      <hr className="entries-section__list-separation-line"></hr>
+    </>
   );
 }
