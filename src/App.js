0224955c6cecf6fda38177b087f83f69e3c36cae
Maria Rios Navarro <propscode254@gmail.com>
2023-05-20 16:07:21 +0200

    Add content to EntryForm.js and change global.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Header from "./components/Header/Header.js";
+import EntryForm from "./components/EntryForm/EntryForm.js";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Header />
-      {/* <Main />
-      <Footer /> */}
+      <main>
+        <EntryForm />
+      </main>
+      {/* <Footer />  */}
     </>
   );
 }
---
+++ src/components/EntryForm/EntryForm.js
@@ -0,0 +1,14 @@
+import "./EntryForm.css";
+
+export default function EntryForm() {
+  return (
+    <form>
+        <label></label>
+        <input></input>
+        <label></label>
+        <textarea></textarea>
+        <button></button>
+
+    </form>
+  );
+}
