0b471304d932cb7562c7591bda492623063e420a
Maria Rios Navarro <propscode254@gmail.com>
2023-05-20 22:41:56 +0200

    CSS TAb und TabBar

--- src/components/Tab/Tab.js
+++ src/components/Tab/Tab.js
@@ -3,7 +3,7 @@ import "./Tab.css";
 export default function Tab({ name, count }) {
   return (
     <li className="tab-bar__tab">
-      {name} <span className="tab-bar__tab-counter">({count})</span>
+      {name} <span className="tab-bar__tab-counter">{count}</span>
     </li>
   );
 }
