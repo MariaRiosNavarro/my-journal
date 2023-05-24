45116672aef29b75a8eedc078948f8e8489f65ea
Maria Rios Navarro <propscode254@gmail.com>
2023-05-24 20:55:16 +0200

    change the image for the favButton

--- src/components/Entry/Entry.js
+++ src/components/Entry/Entry.js
@@ -1,15 +1,12 @@
 import "./Entry.css";
+import FavoriteButton from "../favouriteButton/FavouriteButton";
 
-export default function Entry({ title, date, text, icon }) {
+export default function Entry({ title, date, text }) {
   return (
     <>
       <li className="entries-section__list-item">
         <h3 className="entries-section__list-item__journal-date">{date}</h3>
-        <img
-          className="entries-section__list-item__journal-icon"
-          src={icon}
-          alt="Icon"
-        />
+        <FavoriteButton />
         <h4 className="entries-section__list-item__journal-title">{title}</h4>
         <p className="entries-section__list-item__journal-text">{text}</p>
       </li>
